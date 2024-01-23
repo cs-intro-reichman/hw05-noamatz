f10f9804b3af6d4b2fa3d3b634bdbf7ec0282996
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2024-01-23 11:55:53 +0200

    Final

--- GameOfLife.java
+++ GameOfLife.java
@@ -167,7 +167,11 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for (int i = 1; i < arr.length-1; i++) {
 			for (int j = 1; j < arr[0].length-1; j++) {
-				System.out.print(arr[i][j] + "  ");
+				if (j == arr[0].length - 1) {
+					System.out.print(arr[i][j]);
+				} else {
+					System.out.print(arr[i][j] + "  ");
+				}
 			}
 			System.out.println();
 		}
